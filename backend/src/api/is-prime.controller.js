4190e0416a178543eabced1b7f10cd7165ffb4e0
Matt <lumpychum@gmail.com>
2023-03-05 21:10:33 -0500

    fixed fatal bug in api endpoint

--- backend/src/api/is-prime.controller.js
+++ backend/src/api/is-prime.controller.js
@@ -1,54 +1,51 @@
 import db from '../database.js'
 
+/*
+If num is a valid number it returns a formatted BigInt, else returns false
+*/
+function isValid(num) {
+    num = num.replaceAll(',', '');
+    num = num.replaceAll(' ', '');
+
+    try {
+        let large = BigInt(num);
+
+        if (large <= 2n || large > 18446744073709551615n || large % 1n != 0) {
+            throw new Error(`Invalid number: ${num}`);
+
+        }
+
+        return large;
+
+    } catch (e) {
+        console.log(`Error parsing number: ${e}`);
+        return false;
+
+    }
+
+}
 export default class IsPrimeController {
     static async checkPrime(req, res, next) {
         // make sure num is a valid number (isValid function)
-        let num = this.isValid(req.params.num)
+        let num = isValid(req.params.n)
         
         if (num != false) {
             // access SQL DB to see if prime number has already been calculated
-            let cached = db.checkForNum(num.toString(), (res) => {
-                result = res;
+            let cached = db.checkForNum(num.toString(), (reslt) => {
+                if (reslt.length == 1) {
+                    // if so: return IsPrime value from the sql table
+                    res.json(reslt[0]);
 
-                // if so: return IsPrime value from the sql table
-                if (result.length == 1) {
-                    return res[0]['IsPrime'];
                 } else {
                     // if not: call C binary and return result and foundInDB= false; add to DB
 
                 }
-                
+
             });
         }
 
 
     }
-    
-    /*
-    If num is a valid number it returns a formatted BigInt, else returns false
-    */
-    static isValid(num) {
-        num = num.replaceAll(',', '');
-        num = num.replaceAll(' ', '');
-
-        try {
-            let large = BigInt(num);
-
-            if (large <= 2n || large > 18446744073709551615n || large % 1n != 0) {
-                throw new Error(`Invalid number: ${num}`);
-
-            }
-
-            return large;
-
-        } catch (e) {
-            console.log(`Error parsing number: ${e}`);
-            return false;
-
-        }
-
-    }
-
 }
 
 function test() {
