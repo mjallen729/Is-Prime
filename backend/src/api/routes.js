d04a9a4a40c5e0e72b08d1677827d8acc90afa1d
Matt <lumpychum@gmail.com>
2023-03-06 15:57:30 -0500

    updated add route

--- backend/src/api/routes.js
+++ backend/src/api/routes.js
@@ -4,6 +4,6 @@ import PrimeFind from './is-prime.controller.js'
 const router = express.Router();
 
 router.route('/num/:num').get(PrimeFind.checkPrime);
-router.route('/num/add').post(PrimeFind.add);
+router.route('/add').post(PrimeFind.add);
 
 export default router
