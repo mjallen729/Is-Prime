f26a4a2d7d985644bfadce5acc45f22f44ef7ebd
Matt <lumpychum@gmail.com>
2023-03-10 16:31:05 -0500

    fixed react forcing 2 space indent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1 class="bg-red-400">hello world!</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1 class="bg-red-400">hello world!</h1>
+        </div>
+    );
 }
 
 export default App;
