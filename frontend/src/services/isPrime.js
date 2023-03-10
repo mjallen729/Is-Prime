a7265c16b075001a0fadc6c0867f5ed0a8c91b5e
Matt <lumpychum@gmail.com>
2023-03-10 18:44:41 -0500

    implemented api service

---
+++ frontend/src/services/isPrime.js
@@ -0,0 +1,28 @@
+import fetch from 'node-fetch'
+import dotenv from 'dotenv'
+
+dotenv.config('../../.env');
+let url = 'localhost:5100';
+let header = {'Content-type': 'application/json',
+                'key': process.env.APIKEY};
+
+class IsPrime {
+    checkPrime(num) {
+        return fetch(`${url}/api/num/${num}`, {
+            method: 'GET', 
+            headers: header
+        });
+
+    }
+
+    addPrime(num, isPrime, user) {
+        return fetch(`${url}/api/add?num=${num}&prime=${isPrime}&user=${user}`, {
+            method: 'POST',
+            headers: header
+        });
+
+    }
+
+}
+
+export default new IsPrime();
