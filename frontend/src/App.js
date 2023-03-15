20b870bb5a5b26c289aa628e99ff07b28a2c1b3e
Matt <lumpychum@gmail.com>
2023-03-14 21:50:19 -0400

    implemented functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import {useState} from "react"
+import NumInfo from './components/numInfo.js';
 
 function App() {
     const [inputVal, setInputVal] = useState('Enter a number...');
+    const [curInput, setCurInput] = useState('');
+    const [result, setResult] = useState('');
 
     function resetInput() {
         setInputVal('Enter a number...');
@@ -10,27 +13,52 @@ function App() {
 
     const keyPressEvent = (event) => {
         if (event.key === 'Enter') {
-            console.log('ENTER');
+            console.log('ENTER');  //press the check button!
         }
+    }
+
+    function saveValue(e) {
+        setCurInput(e.target.value)
+
+    }
+
+    function render() {
+        if (curInput.length <= 0) {
+            return;
+
+        }
+
+        setResult(curInput);
+
     }
 
     return (
         <div className="App h-screen">
             <div className="flex h-1/6 bg-blue-700 items-center justify-center">
-                <h1 className="font-bold text-5xl text-white">Prime Finder</h1>
+                <h1 className="font-bold text-4xl md:text-5xl text-white">Prime Finder</h1>
             </div>
 
-            <div id="content" className="flex flex-col flex-wrap bg-red-200 pt-[64px] items-center">
-                <input type="text" placeholder={inputVal} onFocus={(e) => {setInputVal(''); e.target.value = ''; }} onBlur={resetInput}
-                onKeyDown={keyPressEvent} className="w-[450px] h-[55px] text-3xl text-center outline outline-[3px]
-                outline-blue-700 rounded-full"/>
+            <div id="content" className="flex flex-col pt-[64px] items-center">
+                <input id="inputbox" 
+                    type="text" 
+                    placeholder={inputVal} 
+                    onFocus={(e) => {setInputVal(''); e.target.value = '';}}
+                    onBlur={resetInput}
+                    onKeyDown={keyPressEvent}
+                    onChange={e => saveValue(e)}
+                    className="w-[300px] md:w-[450px] h-[55px] text-2xl md:text-3xl text-center" maxLength="20"/>
 
-                <button className="bg-blue-700 text-xl text-center rounded-full text-white 
-                h-[55px] w-[300px] mt-[40px]" id="submitbutton">Check!</button>
+                <button id="submitbutton" 
+                    className="bg-[#001011] text-xl text-center rounded-full 
+                        text-white h-[55px] w-[220px] mt-[40px]"
+                    onClick={render}>
+                        Check!
+                </button>
 
-                <div id="infobox" className="w-[450px] mt-[80px]">
-
+                <div id="infobox" className="w-[325px] md:w-[450px] mt-[80px] text-center">
+                    <NumInfo num={result}/>
                 </div>
+
             </div>
         </div>
     );
