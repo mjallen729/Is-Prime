e0d636db6ad5cf61c2ae0b103aade0e7eb27bd56
Matt <lumpychum@gmail.com>
2023-04-24 13:26:45 -0400

    fixed infinite rendering loop

--- frontend/src/components/numInfo.js
+++ frontend/src/components/numInfo.js
@@ -1,43 +1,45 @@
-import { useState } from "react"
-//color for prime: #395B50
-//color for composite: #FE5E41
+import { useState, useEffect } from "react"
 
 const NumInfo = (props) => {
     const [resultColor, setResultColor] = useState('');
     const [result, setResult] = useState('');
 
-    if (props['error']) {
+    useEffect(() => {
+        if (props.error) {
+            return;
+        }
+
+        if (props.result === 'prime') {
+            setResultColor('395B50');
+            setResult('PRIME');
+            
+        } else if (props.result === 'composite') {
+            setResultColor('FE5E41');
+            setResult('COMPOSITE');
+        }
+    }, [props.error, props.result]);
+
+    if (props.error) {
         return (
             <div id="infocontent">
-                <p className="text-2xl">{props['error']}</p>
+                <p className="text-2xl">{props.error}</p>
             </div>
         );
     }
 
-    if (props['result'] === 'prime') {
-        setResultColor('395B50');
-        setResult('PRIME');
-
-    } else if (props['result'] === 'composite') {
-        setResultColor('FE5E41');
-        setResult('COMPOSITE');
-
-    }
-
     return (
         <div id="infocontent">
-            <p className="text-2xl">{props['num']}</p>
-            <br />
-            <p className="text-xl">
+            <p className="text-3xl">{props.num}</p>
+            <br /><br/>
+            <p className="text-2xl">
                 <span className={`text-[#${resultColor}]`}>{result}</span>
+                <br /><br/>
+                <span>Found By: {props.user}</span>
                 <br />
-                <span>Found By: {props['user']}</span>
-                <br />
-                <span>Date: {props['date']}</span>
+                <span>Date: {props.date}</span>
             </p>
         </div>
     );
-
 }
 
 export default NumInfo;
